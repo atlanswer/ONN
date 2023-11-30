d5f4354d65148ec3c8ade801dca045e309576135
Atlanswer <i@atlanswer.com>
2023-11-30 16:42:53 +0800

    organize functions

--- test.py
+++ test.py
@@ -1,7 +1,27 @@
+# %%
 import chex
 import jax
+import jax.numpy as jnp
+import numpy as np
+from clu import metric_writers, periodic_actions
 
 chex.set_n_cpu_devices(8)
 
-print(f"{jax.devices()}")
-print(f"{jax.device_count()}")
+logdir = "./metrics"
+total_steps = 100
+
+writer = metric_writers.create_default_writer(logdir)
+
+hooks = [
+    periodic_actions.ReportProgress(
+        num_train_steps=total_steps, every_steps=10, writer=writer
+    ),
+    periodic_actions.Profile(logdir=logdir),
+]
+
+for step in range(total_steps):
+    # writer.write_scalars(step, dict(loss=0.9**step))
+    for hook in hooks:
+        hook(step)
+
+#%%
