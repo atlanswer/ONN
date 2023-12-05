6757e0d87a10cae8d5f74fa39be9f2f71a946616
Atlanswer <i@atlanswer.com>
2023-12-05 10:23:15 +0800

    async simulation

--- onn_slotted_patch.py
+++ onn_slotted_patch.py
@@ -1,9 +1,10 @@
 # %% Import
+import asyncio
 import csv  # pyright: ignore
 import os
 import time
 from dataclasses import dataclass
-from queue import Queue
+from pathlib import Path
 from typing import cast
 
 import benchmark_functions as bf  # pyright: ignore
@@ -17,16 +18,19 @@ from antcal.model.slotted_patch import (  # pyright: ignore[reportMissingTypeStu
     N_DIMS_SLOTTED_PATCH,
     VAR_BOUNDS,
     check_constrains,
-    obj_fn_queue,
+    obj_fn,
 )
-from antcal.utils import submit_tasks  # pyright: ignore
+from antcal.pyaedt.hfss import new_hfss_session  # pyright: ignore
+from antcal.utils import submit_tasks  # pyright: ignore[reportMissingTypeStubs]
 from loguru import logger
 from matplotlib import cm  # pyright: ignore
-from antcal.pyaedt.hfss import new_hfss_session  # pyright: ignore[reportMissingTypeStubs]
+from pyaedt.hfss import Hfss  # pyright: ignore[reportMissingTypeStubs]
 
 # %%
 cwd = os.getcwd()
 save_path = os.path.join(cwd, "save")
+save_dir = Path(save_path)
+save_dir.mkdir(exist_ok=True)
 logger.add("log.log")
 
 
@@ -35,11 +39,11 @@ logger.add("log.log")
 @dataclass
 class OptParams:
     n_dims: int = N_DIMS_SLOTTED_PATCH
-    s_init: int = 6
-    n_candidates: int = 3
-    n_new_candidates: int = 3
-    n_predictors: int = 6
-    n_iters: int = 150
+    s_init: int = 2 * N_DIMS_SLOTTED_PATCH
+    n_candidates: int = 2 * N_DIMS_SLOTTED_PATCH
+    n_new_candidates: int = 2 * N_DIMS_SLOTTED_PATCH
+    n_predictors: int = N_DIMS_SLOTTED_PATCH
+    n_iters: int = 200
     alpha_mut_ratio: float = 0.4
     n_dims_mut: int = int(alpha_mut_ratio * n_dims) + 1
     n_opt_runs = 1
@@ -47,6 +51,7 @@ class OptParams:
     LEARNING_RATE: float = 0.01
     N_MODEL_DIMS: int = 100 * n_dims
     EPOCHS: int = 2 * int(s_init / BATCH_SIZE) + 1
+    N_SIMULATORS = 3
 
 
 # %% Functions
@@ -174,15 +179,6 @@ def find_new_candidates(
     return new_candidates
 
 
-# %% AEDT queue
-aedt_queue = Queue()  # pyright: ignore
-h1 = new_hfss_session()
-h2 = new_hfss_session()
-h3 = new_hfss_session()
-aedt_queue.put(h1)
-aedt_queue.put(h2)
-aedt_queue.put(h3)
-
 # %% main
 
 if __name__ == "__main__":
@@ -192,33 +188,52 @@ if __name__ == "__main__":
     hist_x1 = []
     hist_x2 = []
 
-    opt_params = OptParams()
     rng = np.random.default_rng()
+    opt_params = OptParams()
+    time_str = time.strftime(r"%m%d%H%M", time.localtime())
+
+    aedt_queue: asyncio.Queue[Hfss] = asyncio.Queue(maxsize=10)
+    # for _ in range(opt_params.N_SIMULATORS):
+    #     aedt_queue.put_nowait(new_hfss_session())
 
     for i_opt in range(opt_params.n_opt_runs):
         # torch.manual_seed(i_opt + 1)
 
         predictors, optimizers = gen_init_predictors(opt_params)
 
-        X = gen_init_X(opt_params, VAR_BOUNDS, rng)  # pyright: ignore[reportUnboundVariable]
-        Y = submit_tasks(aedt_queue, X, obj_fn_queue)  # pyright: ignore[reportUnboundVariable]
+        X = gen_init_X(opt_params, VAR_BOUNDS, rng)
+        Y = asyncio.run(submit_tasks(obj_fn, X, 3, aedt_queue))
 
         best_ys = [min(Y)]
         idx_best_y = np.argmin(Y)
         best_xs = X[idx_best_y]
         new_candidates, y_new_candidates = X, Y
 
-        # with open(
-        #     os.path.join(save_path, f"opt_{i_opt}.csv"),
-        #     mode="a",
-        #     newline="",
-        # ) as csv_file:
-        #     csv_writer = csv.writer(csv_file)
-        #     header = ["iter", "x1", "x2", "y", "loss"]
-        #     csv_writer.writerow(header)
+        with open(
+            os.path.join(save_path, f"{time_str}.csv"),
+            mode="a",
+            newline="",
+        ) as csv_file:
+            csv_writer = csv.writer(csv_file)
+            header = [
+                "iter",
+                "W",
+                "L",
+                "Wr",
+                "Wu",
+                "Lr",
+                "Pr",
+                "Lh",
+                "Lv",
+                "fx",
+                "fy",
+                "y",
+                "loss",
+            ]
+            csv_writer.writerow(header)
 
         for iter in range(opt_params.n_iters):
-            t_start = time.perf_counter()
+            t_start = time.time()
 
             predictors, optimizers, error = update_predictors(
                 predictors, optimizers, new_candidates, y_new_candidates
@@ -228,7 +243,9 @@ if __name__ == "__main__":
             new_candidates = find_new_candidates(
                 opt_params, X_candidates_mut, predictors, rng
             )
-            y_new_candidates = submit_tasks(aedt_queue, new_candidates, obj_fn_queue)
+            y_new_candidates = asyncio.run(
+                submit_tasks(obj_fn, new_candidates, 3, aedt_queue)
+            )
 
             X = np.vstack([X, new_candidates])  # pyright: ignore[reportConstantRedefinition]
             Y = np.hstack([Y, y_new_candidates])  # pyright: ignore[reportConstantRedefinition]
@@ -242,31 +259,36 @@ if __name__ == "__main__":
             ) * opt_params.alpha_mut_ratio
             n_dims_mut = int(alpha_mutation * opt_params.n_dims) + 1
 
-            # with open(
-            #     os.path.join(save_path, f"opt_{i_opt}.csv"),
-            #     mode="a",
-            #     newline="",
-            # ) as csv_file:
-            #     csv_writer = csv.writer(csv_file)
-            #     [x1, x2] = best_xs[-1]
-            #     y = best_ys[-1]
-            #     row = [iter, x1, x2, y, f"{error}"]
-            #     csv_writer.writerow(row)
-
-            if iter % 25 == 0:
-                logger.info(f"iter: {iter}, error: {error}")
-                logger.info(f"x: {best_xs[-1]}, y: {best_ys[-1]}")
+            with open(
+                os.path.join(save_path, f"{time_str}.csv"),
+                mode="a",
+                newline="",
+            ) as csv_file:
+                csv_writer = csv.writer(csv_file)
+                x = best_xs[-1]
+                y = best_ys[-1]
+                row = [iter, *x, y, f"{error}"]
+                csv_writer.writerow(row)
 
             hist_y.append(best_ys[-1])
             hist_loss.append(error.item())
 
-            t_finish = time.perf_counter()
-            iter_duration.append(t_finish - t_start)
+            t_finish = time.time()
+            t_duration = int(t_finish - t_start)
+            logger.debug(
+                f"iter: {iter} | y: {y} | error: {error} | duration: {t_duration} s."
+            )
+            iter_duration.append(t_duration)
 
         logger.info(f"duration: {iter_duration} (s)")
 
-        hist_x1 = [x[0] for x in X]
-        hist_x2 = [x[1] for x in X]
+        # hist_x1 = [x[0] for x in X]
+        # hist_x2 = [x[1] for x in X]
+
+    while not aedt_queue.empty():
+        hfss = aedt_queue.get_nowait()
+        hfss.close_desktop()
+
 
 # %% Graph y
 
